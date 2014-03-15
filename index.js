a5af6234df3ca9a8cfcbd29cad42328e98ad4007
yosuke-furukawa <yosuke.furukawa@gmail.com>
2014-03-15 13:40:11 +0900

    chat sample

--- index.js
+++ index.js
@@ -2,25 +2,42 @@ var Primus = require('primus');
 var http = require('http');
 var koa = require('koa');
 var path = require('path')
-var staticCache = require('koa-static-cache')
+var serve = require('koa-static')
+var validator = require('validator');
 var app = koa();
 
-
-
-app.use(staticCache(path.join(__dirname, 'public'), {
-    maxAge: 365 * 24 * 60 * 60
-}))
+app.use(serve("public"));
 
 var server = http.createServer(app.callback());
 var primus = new Primus(server);
 
 
+// setting primus/primus.js
 primus.library();
 
+// connect hook
 primus.on('connection', function (spark) {
   console.log("connection");
+  // spark is the new connection
+  console.log("spark headers : ", spark.headers);
+  console.log("spark address : ", spark.address);
+  // query string sockjs not supported
+  console.log("spark query : ", spark.query);
+  // spark id can get client id
+  console.log("spark id  : ", spark.id);
+
+  // send Connect message to one connect client
+  spark.write("CONNECT!!!");
+  // receive data
+  spark.on("data", function(data) {
+    console.log(data);
+    // broadcast all client
+    primus.write(validator.escape(data));
+  });
 });
 
+
+// disconnect hook
 primus.on('disconnection', function (spark) {
   console.log("disconnection");
 });
